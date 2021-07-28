3b818212474ab8939a8dd85dd4a1b12739b1255e
Dianne <mdianne.gabriel@gmail.com>
2021-07-28 16:53:49 -0400

    Pair programmed with Ruchi and Pooja

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,16 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+const transposed = letters[0].map((item, index) => letters.map(row => row[index]))
+    const verticalJoin = transposed.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word))
+        return true;
+    }
+    return false;
+} 
 
-module.exports = wordSearch
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
+
+module.exports = wordSearch;
